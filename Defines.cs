26a3e6eb71cf40f6082e8a0782e404cf79b30b62
TriggerWinkle <kyliandekker2000@gmail.com>
2023-02-08 23:54:49 +0100

    Added editor build configuration to separate it from unity builds

--- Defines.cs
+++ Defines.cs
@@ -1,11 +1,5 @@
 namespace USubtitles.Editor
 {
-	public static class SubtitleEditorVariables
-	{
-		public const float Version = 1.3f;
-		public static Preferences Preferences = new Preferences();
-	}
-
 	public enum InteractionType
 	{
 		TimelineInteraction_None,
---
+++ Editor/WaveEditor.cs
@@ -0,0 +1,15 @@
+namespace USubtitles.Editor
+{
+	public static class SubtitleEditorVariables
+	{
+		public static float Version = 1.2f;
+		public static Preferences Preferences = new Preferences();
+	}
+
+	public enum TimelineInteraction
+	{
+		TimelineInteraction_None,
+		TimelineInteraction_Marker,
+		TimelineInteraction_Time
+	}
+}
